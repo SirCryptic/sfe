ad04441f16f4ccb09578e7a72060348367078b84
sircryptic <48811414+SirCryptic@users.noreply.github.com>
2020-07-16 18:35:52 +0100

    Update sfe.sh

--- sfe.sh
+++ sfe.sh
@@ -11,7 +11,8 @@ echo -e '\e[0m\e[3;36m-------------------------------
 \e[3;31m SpiderFoot EasyLaunch \e[3;36m
 ------------------------------- \e[3;39m
 (1)  Launch Spider Foot Local Host
-(2)  Download & Install SpiderFoot \e[3;36m
+(2)  Download & Install SpiderFoot
+(3)  Remove & Install SpiderFoot \e[3;36m
 ------------------------------- \e[3;39m
        CTRL + C To Exit \e[3;36m
 -------------------------------'
@@ -21,5 +22,6 @@ read -p      " └─────► " n
 case $n in
     1) clear&&echo "launching spiderfoot on localhost"&&cd spiderfoot&&python3 sf.py -l 127.0.0.1:5001;;
     2) cd&&git clone https://github.com/smicallef/spiderfoot.git&&cd spiderfoot&&pip3 install -r requirements.txt&&firefox "https://www.spiderfoot.net/documentation/";;
+    3) cd&&clear&&echo "make sure to make backups of your config/keys"&&read -p "Have You Made Backups? Press Enter To Continue"&&sudo rm -r spiderfoot&&git clone https://github.com/smicallef/spiderfoot.git&&cd spiderfoot&&pip3 install -r requirements.txt&&firefox "https://www.spiderfoot.net/documentation/";;
     
 esac
